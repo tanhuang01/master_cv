773ae5105473ff5e28f075a31c33e54e9ec7aa62
congguangzi <congspark@gmail.com>
2018-10-25 10:43:11 +0800

    modify the code of the SwapTextView

--- app/src/main/java/com/congguangzi/master_cv/views/_17_CustomTextWithPicView.java
+++ app/src/main/java/com/congguangzi/master_cv/views/_17_CustomTextWithPicView.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.GuardedBy;
 import android.support.annotation.Nullable;
 import android.text.Layout;
 import android.text.StaticLayout;
@@ -35,7 +36,14 @@ public class _17_CustomTextWithPicView extends View {
     float textHeight;
     float[] measureWidth = new float[1];
 
-    private final String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    private final String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+            "when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+            "It has survived not only five centuries, but also the leap into electronic typesetting, " +
+            "remaining essentially unchanged. " +
+            "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
+            "and more recently with desktop publishing software like " +
+            "Aldus PageMaker including versions of Lorem Ipsum.";
 
     StaticLayout staticLayout;
 
